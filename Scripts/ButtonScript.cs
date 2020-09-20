534cd34c1fecd22d700ca1b7f87e256fe6f0caf6
mln-grge <mgeorge00747@gmail.com>
2020-09-20 19:30:10 +0530

    master

---
+++ Scripts/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class ButtonScript : MonoBehaviour
+{
+    public Button button;
+    public string newScene;
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        Debug.Log("ButtonPressed");
+        SceneManager.LoadScene(newScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scripts/MyFirstScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("My First Script Awake function");
+    }
+    void Start()
+    {
+        Debug.Log("My First Script Start function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetMouseButtonDown(0)) 
+        {
+            Debug.Log("Update Input F is pressed");
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("Update Input F");
+        }
+
+        if (Input.GetKeyUp(KeyCode.F)) 
+        {
+            Debug.Log("Update Input F is released");
+        }
+    }
+
+}
